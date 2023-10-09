e4078524d83d8e3e98c61a74717e6d35b0cc3124
예디 <twoye1193@ajou.ac.kr>
2023-10-09 21:59:07 +0900

    dev setting

---
+++ src/main/java/org/gmboard/gmboard/GmboardBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gmboard.gmboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmboardBackendApplication.class, args);
+    }
+
+}
